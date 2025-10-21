4e122e03f4405cd434e5786e6835e5dbd1f7b287
Mihai-did-it <mihailache100@gmail.com>
2025-10-20 21:17:20 -0700

    helloooooo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const viewport: Viewport = {
   maximumScale: 1,
   userScalable: false,
   viewportFit: 'cover',
-  themeColor: '#020617', // Darkest color - matches notch area
+  themeColor: '#020617', // Darkest color - matches notch areaasd
 }
 
 export default function RootLayout({
